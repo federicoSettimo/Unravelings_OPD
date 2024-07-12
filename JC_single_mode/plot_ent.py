0e82f2c8a79aa7ffc46da9fc109574f897b38abd
Federico Settimo <fede.settimo@gmail.com>
2024-07-12 15:11:12 +0200

    JC zero discord

--- JC_single_mode/plot_ent.py
+++ JC_single_mode/plot_ent.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
 # Function to read data from a file
@@ -90,8 +91,6 @@ avg_Phi_z_Q_z = .5*(avg_Phi_z_Q_z_p - avg_Phi_z_Q_z_m)
 err_Phi_z_Q_z = .5*np.sqrt(err_Phi_z_Q_z_p**2 + err_Phi_z_Q_z_m**2)
 exact_Phi_z_Q_z = .5*(exact_Phi_z_Q_z_p - exact_Phi_z_Q_z_m)
 
-markevery = int(len(avg_Phi_x_Q_z_p)/50)
-markersize=4
 params = open("params.txt")
 tmax = float(params.readline())
 t = np.linspace(0, tmax, avg_Phi_x_Q_z_p.shape[0])
@@ -100,8 +99,10 @@ t = np.linspace(0, tmax, avg_Phi_x_Q_z_p.shape[0])
 fig, ax = plt.subplots(1, 2, figsize=(15, 7), sharex=True, sharey = True)
 fig.tight_layout()
 
-colors = ["blue", "red", "green", "yellow", "black"]
+colors = sns.color_palette("muted", 8)
 markers = ['o', 'x', '^', 's', 'd']
+markevery = int(len(avg_Phi_x_Q_z_p)/50)
+markersize=5.5
 
 
 ax[0].plot(t, exact_Phi_x_Q_0, color=colors[0], label=r'$\Phi^0_t[Q_0]$', marker=markers[0], markersize=markersize, markevery=len(t))
@@ -113,26 +114,11 @@ ax[0].errorbar(t, avg_Phi_x_Q_y, err_Phi_x_Q_y, color=colors[2], marker=markers[
 ax[0].plot(t, exact_Phi_z_Q_z, color=colors[3], label=r'$\Phi^z_t[Q_z]$', marker=markers[3], markersize=markersize, markevery=len(t))
 ax[0].errorbar(t, avg_Phi_z_Q_z, err_Phi_z_Q_z, color=colors[3], marker=markers[3], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
-if False:
-    ax[0].plot(t, exact_Phi_x_Q_0_p, color='black', label=r'$\Phi^0_t[Q_0^+]$')
-    ax[0].errorbar(t, avg_Phi_x_Q_0_p, err_Phi_x_Q_0_p, color='black', marker='o', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
-    ax[0].plot(t, exact_Phi_x_Q_x_p, color='red', label=r'$\Phi^x_t[Q_x^+]$')
-    ax[0].errorbar(t, avg_Phi_x_Q_x_p, err_Phi_x_Q_x_p, color='red', marker='o', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
-    ax[0].plot(t, exact_Phi_x_Q_y_p, color='green', label=r'$\Phi^y_t[Q_y^+]$')
-    ax[0].errorbar(t, avg_Phi_x_Q_y_p, err_Phi_x_Q_y_p, color='green', marker='o', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
-    ax[0].plot(t, exact_Phi_z_Q_z_p, color='blue', label=r'$\Phi^z_t[Q_z^+]$')
-    ax[0].errorbar(t, avg_Phi_z_Q_z_p, err_Phi_z_Q_z_p, color='blue', marker='o', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
-
-    ax[1].plot(t, exact_Phi_x_Q_0_m, color='black', label=r'$\Phi^0_t[Q_0^-]$')
-    ax[1].errorbar(t, avg_Phi_x_Q_0_m, err_Phi_x_Q_0_m, color='black', marker='o', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
-    ax[1].plot(t, exact_Phi_x_Q_x_m, color='red', label=r'$\Phi^x_t[Q_x^-]$')
-    ax[1].errorbar(t, avg_Phi_x_Q_x_m, err_Phi_x_Q_x_m, color='red', marker='o', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
-    ax[1].plot(t, exact_Phi_x_Q_y_m, color='green', label=r'$\Phi^y_t[Q_y^-]$')
-    ax[1].errorbar(t, avg_Phi_x_Q_y_m, err_Phi_x_Q_y_m, color='green', marker='o', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
-    ax[1].plot(t, exact_Phi_z_Q_z_m, color='blue', label=r'$\Phi^x_t[Q_z^-]$')
-    ax[1].errorbar(t, avg_Phi_z_Q_z_m, err_Phi_z_Q_z_m, color='blue', marker='o', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
 
+
+
+#colors = sns.color_palette("husl")
 
 # Now rho_S and some repreparations
 w_0 = 1
@@ -142,8 +128,8 @@ w_z = 1
 exact_rho_S = w_0*exact_Phi_x_Q_0 + w_x*exact_Phi_x_Q_x + w_y*exact_Phi_x_Q_y + w_z*exact_Phi_z_Q_z
 avg_rho_S = w_0*avg_Phi_x_Q_0 + w_x*avg_Phi_x_Q_x + w_y*avg_Phi_x_Q_y + w_z*avg_Phi_z_Q_z
 err_rho_S = .25*np.sqrt(err_Phi_x_Q_0**2 + err_Phi_x_Q_x**2 + err_Phi_x_Q_y**2 + err_Phi_z_Q_z**2)
-ax[1].plot(t, exact_rho_S, color=colors[4], label=r'$\mathcal{R}=\operatorname{id}$', marker=markers[4], markersize=markersize, markevery=len(t))
-ax[1].errorbar(t, avg_rho_S, err_rho_S, color=colors[4], marker=markers[4], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
+ax[1].plot(t, exact_rho_S, color=colors[4], label=r'$\mathcal{R}=\operatorname{id}$', marker=markers[0], markersize=markersize, markevery=len(t))
+ax[1].errorbar(t, avg_rho_S, err_rho_S, color=colors[4], marker=markers[0], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
 # R[X] = \sigma_x X \sigma_x
 r00 = 1
@@ -154,8 +140,8 @@ ryy = -1
 rzz = -1
 exact_R1 = w_0*(r00*exact_Phi_x_Q_0 + r0y*exact_Phi_x_Q_y + r0z*exact_Phi_x_Q_z) + w_x*rxx*exact_Phi_x_Q_x + w_y*ryy*exact_Phi_x_Q_y + w_z*rzz*exact_Phi_z_Q_z;
 avg_R1 = w_0*(r00*avg_Phi_x_Q_0 + r0y*avg_Phi_x_Q_y + r0z*avg_Phi_x_Q_z) + w_x*rxx*avg_Phi_x_Q_x + w_y*ryy*avg_Phi_x_Q_y + w_z*rzz*avg_Phi_z_Q_z;
-ax[1].plot(t, exact_R1, color=colors[0], label=r'$\mathcal{R}=\sigma_x\cdot\sigma_x$', marker=markers[0], markersize=markersize, markevery=len(t))
-ax[1].errorbar(t, avg_R1, err_rho_S, color=colors[0], marker=markers[0], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
+ax[1].plot(t, exact_R1, color=colors[5], label=r'$\mathcal{R}=\sigma_x\cdot\sigma_x$', marker=markers[1], markersize=markersize, markevery=len(t))
+ax[1].errorbar(t, avg_R1, err_rho_S, color=colors[5], marker=markers[1], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
 # R[X] = <0|X|0> |-><-|
 if False:
@@ -179,8 +165,8 @@ r0x = 1
 r0y = 1
 exact_R2 = (w_0*(r00*exact_Phi_x_Q_0 + r0x*exact_Phi_x_Q_x + r0y*exact_Phi_x_Q_y) + w_z*(rz0*exact_Phi_z_Q_0 + rzx*exact_Phi_z_Q_x + rzy*exact_Phi_z_Q_y))/(.5)
 avg_R2 = (w_0*(r00*avg_Phi_x_Q_0 + r0x*avg_Phi_x_Q_x + r0y*avg_Phi_x_Q_y) + w_z*(rz0*avg_Phi_z_Q_0 + rzx*avg_Phi_z_Q_x + rzy*avg_Phi_z_Q_y))/(.5)
-ax[1].plot(t, exact_R2, color=colors[1], label=r'$\mathcal{R}=\langle0\vert\cdot\vert0\rangle\,\vert0\rangle\langle0\vert$', marker=markers[1], markersize=markersize, markevery=len(t))
-ax[1].errorbar(t, avg_R2, err_rho_S, color=colors[1], marker=markers[1], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
+ax[1].plot(t, exact_R2, color=colors[6], label=r'$\mathcal{R}=\langle0\vert\cdot\vert0\rangle\,\vert0\rangle\langle0\vert$', marker=markers[2], markersize=markersize, markevery=len(t))
+ax[1].errorbar(t, avg_R2, err_rho_S, color=colors[6], marker=markers[2], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
 # R[X] = <1|X|1> |1><1|
 rz0 = .5
@@ -189,8 +175,8 @@ rzy = .5
 rzz = 1
 exact_R3 = w_z*(rz0*exact_Phi_z_Q_0 + rzx*exact_Phi_z_Q_x + rzy*exact_Phi_z_Q_y + rzz*exact_Phi_z_Q_z)/(.5)
 avg_R3 = w_z*(rz0*avg_Phi_z_Q_0 + rzx*avg_Phi_z_Q_x + rzy*avg_Phi_z_Q_y + rzz*avg_Phi_z_Q_z)/(.5)
-ax[1].plot(t, exact_R3, color=colors[2], label=r'$\mathcal{R}=\langle1\vert\cdot\vert1\rangle\,\vert1\rangle\langle1\vert$', marker=markers[2], markersize=markersize, markevery=len(t))
-ax[1].errorbar(t, avg_R3, err_rho_S, color=colors[2], marker=markers[2], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
+ax[1].plot(t, exact_R3, color=colors[7], label=r'$\mathcal{R}=\langle1\vert\cdot\vert1\rangle\,\vert1\rangle\langle1\vert$', marker=markers[3], markersize=markersize, markevery=len(t))
+ax[1].errorbar(t, avg_R3, err_rho_S, color=colors[7], marker=markers[3], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
 
 
