ff6cf4949990c6883a0ac2dbb641f39167c506b8
Federico Settimo <fede.settimo@gmail.com>
2025-02-04 12:21:58 +0200

    JC single mode, plotting for entangled initial state

--- JC_single_mode/plot_ent.py
+++ JC_single_mode/plot_ent.py
@@ -91,6 +91,90 @@ avg_Phi_z_Q_z = .5*(avg_Phi_z_Q_z_p - avg_Phi_z_Q_z_m)
 err_Phi_z_Q_z = .5*np.sqrt(err_Phi_z_Q_z_p**2 + err_Phi_z_Q_z_m**2)
 exact_Phi_z_Q_z = .5*(exact_Phi_z_Q_z_p - exact_Phi_z_Q_z_m)
 
+# Now reading zero discord
+avg_Phi_0_Q_0_p_0d = read_file("avg_Phi_0_Q_0_+_zero_discord.txt")
+err_Phi_0_Q_0_p_0d = read_file("err_obs_Phi_0_Q_0_+_zero_discord.txt")
+exact_Phi_0_Q_0_p_0d = read_file("exact_Phi_0_Q_0_+_zero_discord.txt")
+avg_Phi_0_Q_x_p_0d = read_file("avg_Phi_0_Q_x_+_zero_discord.txt")
+err_Phi_0_Q_x_p_0d = read_file("err_obs_Phi_0_Q_x_+_zero_discord.txt")
+exact_Phi_0_Q_x_p_0d = read_file("exact_Phi_0_Q_x_+_zero_discord.txt")
+avg_Phi_0_Q_y_p_0d = read_file("avg_Phi_0_Q_y_+_zero_discord.txt")
+err_Phi_0_Q_y_p_0d = read_file("err_obs_Phi_0_Q_y_+_zero_discord.txt")
+exact_Phi_0_Q_y_p_0d = read_file("exact_Phi_0_Q_y_+_zero_discord.txt")
+avg_Phi_0_Q_z_p_0d = read_file("avg_Phi_0_Q_z_+_zero_discord.txt")
+err_Phi_0_Q_z_p_0d = read_file("err_obs_Phi_0_Q_z_+_zero_discord.txt")
+exact_Phi_0_Q_z_p_0d = read_file("exact_Phi_0_Q_z_+_zero_discord.txt")
+
+avg_Phi_0_Q_0_m_0d = read_file("avg_Phi_0_Q_0_-_zero_discord.txt")
+err_Phi_0_Q_0_m_0d = read_file("err_obs_Phi_0_Q_0_-_zero_discord.txt")
+exact_Phi_0_Q_0_m_0d = read_file("exact_Phi_0_Q_0_-_zero_discord.txt")
+avg_Phi_0_Q_x_m_0d = read_file("avg_Phi_0_Q_x_-_zero_discord.txt")
+err_Phi_0_Q_x_m_0d = read_file("err_obs_Phi_0_Q_x_-_zero_discord.txt")
+exact_Phi_0_Q_x_m_0d = read_file("exact_Phi_0_Q_x_-_zero_discord.txt")
+avg_Phi_0_Q_y_m_0d = read_file("avg_Phi_0_Q_y_-_zero_discord.txt")
+err_Phi_0_Q_y_m_0d = read_file("err_obs_Phi_0_Q_y_-_zero_discord.txt")
+exact_Phi_0_Q_y_m_0d = read_file("exact_Phi_0_Q_y_-_zero_discord.txt")
+avg_Phi_0_Q_z_m_0d = read_file("avg_Phi_0_Q_z_-_zero_discord.txt")
+err_Phi_0_Q_z_m_0d = read_file("err_obs_Phi_0_Q_z_-_zero_discord.txt")
+exact_Phi_0_Q_z_m_0d = read_file("exact_Phi_0_Q_z_-_zero_discord.txt")
+
+mu_0_p = (np.sqrt(3)+1)/2
+mu_0_m = (np.sqrt(3)-1)/2
+avg_Phi_0_tilde_Q_0_0d = mu_0_p*avg_Phi_0_Q_0_p_0d - mu_0_m*avg_Phi_0_Q_0_m_0d
+err_Phi_0_tilde_Q_0_0d = .5*np.sqrt(err_Phi_0_Q_0_p_0d**2 + err_Phi_0_Q_0_m_0d**2)
+exact_Phi_0_tilde_Q_0_0d = mu_0_p*exact_Phi_0_Q_0_p_0d - mu_0_m*exact_Phi_0_Q_0_m_0d
+avg_Phi_0_tilde_Q_x_0d = .5*(avg_Phi_0_Q_x_p_0d - avg_Phi_0_Q_x_m_0d)
+err_Phi_0_tilde_Q_x_0d = .5*np.sqrt(err_Phi_0_Q_x_p_0d**2 + err_Phi_0_Q_x_m_0d**2)
+exact_Phi_0_tilde_Q_x_0d = .5*(exact_Phi_0_Q_x_p_0d - exact_Phi_0_Q_x_m_0d)
+avg_Phi_0_tilde_Q_y_0d = .5*(avg_Phi_0_Q_y_p_0d - avg_Phi_0_Q_y_m_0d)
+err_Phi_0_tilde_Q_y_0d = .5*np.sqrt(err_Phi_0_Q_y_p_0d**2 + err_Phi_0_Q_y_m_0d**2)
+exact_Phi_0_tilde_Q_y_0d = .5*(exact_Phi_0_Q_y_p_0d - exact_Phi_0_Q_y_m_0d)
+avg_Phi_0_tilde_Q_z_0d = .5*(avg_Phi_0_Q_z_p_0d - avg_Phi_0_Q_z_m_0d)
+err_Phi_0_tilde_Q_z_0d = .5*np.sqrt(err_Phi_0_Q_z_p_0d**2 + err_Phi_0_Q_z_m_0d**2)
+exact_Phi_0_tilde_Q_z_0d = .5*(exact_Phi_0_Q_z_p_0d - exact_Phi_0_Q_z_m_0d)
+
+# Then: evol with Phi^z
+avg_Phi_1_Q_0_p_0d = read_file("avg_Phi_1_Q_0_+_zero_discord.txt")
+err_Phi_1_Q_0_p_0d = read_file("err_obs_Phi_1_Q_0_+_zero_discord.txt")
+exact_Phi_1_Q_0_p_0d = read_file("exact_Phi_1_Q_0_+_zero_discord.txt")
+avg_Phi_1_Q_x_p_0d = read_file("avg_Phi_1_Q_x_+_zero_discord.txt")
+err_Phi_1_Q_x_p_0d = read_file("err_obs_Phi_1_Q_x_+_zero_discord.txt")
+exact_Phi_1_Q_x_p_0d = read_file("exact_Phi_1_Q_x_+_zero_discord.txt")
+avg_Phi_1_Q_y_p_0d = read_file("avg_Phi_1_Q_y_+_zero_discord.txt")
+err_Phi_1_Q_y_p_0d = read_file("err_obs_Phi_1_Q_y_+_zero_discord.txt")
+exact_Phi_1_Q_y_p_0d = read_file("exact_Phi_1_Q_y_+_zero_discord.txt")
+avg_Phi_1_Q_z_p_0d = read_file("avg_Phi_1_Q_z_+_zero_discord.txt")
+err_Phi_1_Q_z_p_0d = read_file("err_obs_Phi_1_Q_z_+_zero_discord.txt")
+exact_Phi_1_Q_z_p_0d = read_file("exact_Phi_1_Q_z_+_zero_discord.txt")
+
+avg_Phi_1_Q_0_m_0d = read_file("avg_Phi_1_Q_0_-_zero_discord.txt")
+err_Phi_1_Q_0_m_0d = read_file("err_obs_Phi_1_Q_0_-_zero_discord.txt")
+exact_Phi_1_Q_0_m_0d = read_file("exact_Phi_1_Q_0_-_zero_discord.txt")
+avg_Phi_1_Q_x_m_0d = read_file("avg_Phi_1_Q_x_-_zero_discord.txt")
+err_Phi_1_Q_x_m_0d = read_file("err_obs_Phi_1_Q_x_-_zero_discord.txt")
+exact_Phi_1_Q_x_m_0d = read_file("exact_Phi_1_Q_x_-_zero_discord.txt")
+avg_Phi_1_Q_y_m_0d = read_file("avg_Phi_1_Q_y_-_zero_discord.txt")
+err_Phi_1_Q_y_m_0d = read_file("err_obs_Phi_1_Q_y_-_zero_discord.txt")
+exact_Phi_1_Q_y_m_0d = read_file("exact_Phi_1_Q_y_-_zero_discord.txt")
+avg_Phi_1_Q_z_m_0d = read_file("avg_Phi_1_Q_z_-_zero_discord.txt")
+err_Phi_1_Q_z_m_0d = read_file("err_obs_Phi_1_Q_z_-_zero_discord.txt")
+exact_Phi_1_Q_z_m_0d = read_file("exact_Phi_1_Q_z_-_zero_discord.txt")
+
+avg_Phi_1_Q_0_0d = mu_0_p*avg_Phi_1_Q_0_p_0d - mu_0_m*avg_Phi_1_Q_0_m_0d
+err_Phi_1_Q_0_0d = .5*np.sqrt(err_Phi_1_Q_0_p_0d**2 + err_Phi_1_Q_0_m_0d**2)
+exact_Phi_1_Q_0_0d = mu_0_p*exact_Phi_1_Q_0_p_0d - mu_0_m*exact_Phi_1_Q_0_m_0d
+avg_Phi_1_Q_x_0d = .5*(avg_Phi_1_Q_x_p_0d - avg_Phi_1_Q_x_m_0d)
+err_Phi_1_Q_x_0d = .5*np.sqrt(err_Phi_1_Q_x_p_0d**2 + err_Phi_1_Q_x_m_0d**2)
+exact_Phi_1_Q_x_0d = .5*(exact_Phi_1_Q_x_p_0d - exact_Phi_1_Q_x_m_0d)
+avg_Phi_1_Q_y_0d = .5*(avg_Phi_1_Q_y_p_0d - avg_Phi_1_Q_y_m_0d)
+err_Phi_1_Q_y_0d = .5*np.sqrt(err_Phi_1_Q_y_p_0d**2 + err_Phi_1_Q_y_m_0d**2)
+exact_Phi_1_Q_y_0d = .5*(exact_Phi_1_Q_y_p_0d - exact_Phi_1_Q_y_m_0d)
+avg_Phi_1_Q_z_0d = .5*(avg_Phi_1_Q_z_p_0d - avg_Phi_1_Q_z_m_0d)
+err_Phi_1_Q_z_0d = .5*np.sqrt(err_Phi_1_Q_z_p_0d**2 + err_Phi_1_Q_z_m_0d**2)
+exact_Phi_1_Q_z_0d = .5*(exact_Phi_1_Q_z_p_0d - exact_Phi_1_Q_z_m_0d)
+
+
+
 params = open("params.txt")
 tmax = float(params.readline())
 t = np.linspace(0, tmax, avg_Phi_x_Q_z_p.shape[0])
@@ -99,9 +183,10 @@ t = np.linspace(0, tmax, avg_Phi_x_Q_z_p.shape[0])
 fig, ax = plt.subplots(1, 2, figsize=(15, 7), sharex=True, sharey = True)
 fig.tight_layout()
 
-colors = sns.color_palette("muted", 8)
+#colors = sns.color_palette("muted", 4)
+colors = ["goldenrod", "navy", "red", "darkgreen"]
 markers = ['o', 'x', '^', 's', 'd']
-markevery = int(len(avg_Phi_x_Q_z_p)/50)
+markevery = int(len(avg_Phi_x_Q_z_p)/30)
 markersize=5.5
 
 
@@ -119,6 +204,7 @@ ax[0].errorbar(t, avg_Phi_z_Q_z, err_Phi_z_Q_z, color=colors[3], marker=markers[
 
 
 #colors = sns.color_palette("husl")
+colors = ["navy", "red", "darkgreen"]
 
 # Now rho_S and some repreparations
 w_0 = 1
@@ -128,55 +214,58 @@ w_z = 1
 exact_rho_S = w_0*exact_Phi_x_Q_0 + w_x*exact_Phi_x_Q_x + w_y*exact_Phi_x_Q_y + w_z*exact_Phi_z_Q_z
 avg_rho_S = w_0*avg_Phi_x_Q_0 + w_x*avg_Phi_x_Q_x + w_y*avg_Phi_x_Q_y + w_z*avg_Phi_z_Q_z
 err_rho_S = .25*np.sqrt(err_Phi_x_Q_0**2 + err_Phi_x_Q_x**2 + err_Phi_x_Q_y**2 + err_Phi_z_Q_z**2)
-ax[1].plot(t, exact_rho_S, color=colors[4], label=r'$\mathcal{R}=\operatorname{id}$', marker=markers[0], markersize=markersize, markevery=len(t))
-ax[1].errorbar(t, avg_rho_S, err_rho_S, color=colors[4], marker=markers[0], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
+ax[1].plot(t, exact_rho_S, color=colors[0], label=r'$\mathcal{R}=\operatorname{id}$', marker=markers[0], markersize=markersize, markevery=len(t))
+ax[1].errorbar(t, avg_rho_S, err_rho_S, color=colors[0], marker=markers[0], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
-# R[X] = \sigma_x X \sigma_x
-r00 = 1
-r0y = 2
-r0z = 2
-rxx = 1
-ryy = -1
-rzz = -1
-exact_R1 = w_0*(r00*exact_Phi_x_Q_0 + r0y*exact_Phi_x_Q_y + r0z*exact_Phi_x_Q_z) + w_x*rxx*exact_Phi_x_Q_x + w_y*ryy*exact_Phi_x_Q_y + w_z*rzz*exact_Phi_z_Q_z;
-avg_R1 = w_0*(r00*avg_Phi_x_Q_0 + r0y*avg_Phi_x_Q_y + r0z*avg_Phi_x_Q_z) + w_x*rxx*avg_Phi_x_Q_x + w_y*ryy*avg_Phi_x_Q_y + w_z*rzz*avg_Phi_z_Q_z;
-ax[1].plot(t, exact_R1, color=colors[5], label=r'$\mathcal{R}=\sigma_x\cdot\sigma_x$', marker=markers[1], markersize=markersize, markevery=len(t))
-ax[1].errorbar(t, avg_R1, err_rho_S, color=colors[5], marker=markers[1], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
+# R 0 discord - p = 0.5
+p = .5
+avg_Phi_x_Q_0_0d = p*avg_Phi_0_tilde_Q_0_0d + (1-p)*avg_Phi_1_Q_0_0d
+err_Phi_x_Q_0_0d = p*err_Phi_0_tilde_Q_0_0d + (1-p)*err_Phi_1_Q_0_0d
+exact_Phi_x_Q_0_0d = p*exact_Phi_0_tilde_Q_0_0d + (1-p)*exact_Phi_1_Q_0_0d
+avg_Phi_x_Q_x_0d = p*avg_Phi_0_tilde_Q_x_0d + (1-p)*avg_Phi_1_Q_x_0d
+err_Phi_x_Q_x_0d = p*err_Phi_0_tilde_Q_x_0d + (1-p)*err_Phi_1_Q_x_0d
+exact_Phi_x_Q_x_0d = p*exact_Phi_0_tilde_Q_x_0d + (1-p)*exact_Phi_1_Q_x_0d
+avg_Phi_x_Q_y_0d = p*avg_Phi_0_tilde_Q_y_0d + (1-p)*avg_Phi_1_Q_y_0d
+err_Phi_x_Q_y_0d = p*err_Phi_0_tilde_Q_y_0d + (1-p)*err_Phi_1_Q_y_0d
+exact_Phi_x_Q_y_0d = p*exact_Phi_0_tilde_Q_y_0d + (1-p)*exact_Phi_1_Q_y_0d
+avg_Phi_z_Q_z_0d = avg_Phi_1_Q_z_0d
+err_Phi_z_Q_z_0d = err_Phi_1_Q_z_0d
+exact_Phi_z_Q_z_0d = exact_Phi_1_Q_z_0d
+exact_rho_S = w_0*exact_Phi_x_Q_0_0d + w_x*exact_Phi_x_Q_x_0d + w_y*exact_Phi_x_Q_y_0d + w_z*exact_Phi_z_Q_z_0d
+avg_rho_S = w_0*avg_Phi_x_Q_0_0d + w_x*avg_Phi_x_Q_x_0d + w_y*avg_Phi_x_Q_y_0d + w_z*avg_Phi_z_Q_z_0d
+err_rho_S = .25*np.sqrt(err_Phi_x_Q_0_0d**2 + err_Phi_x_Q_x_0d**2 + err_Phi_x_Q_y_0d**2 + err_Phi_z_Q_z_0d**2)
+ax[1].plot(t, exact_rho_S, color=colors[1], label=r'$\mathcal{R}_0^{0.5}$', marker=markers[1], markersize=markersize, markevery=len(t))
+ax[1].errorbar(t, avg_rho_S, err_rho_S, color=colors[1], marker=markers[1], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
-# R[X] = <0|X|0> |-><-|
-if False:
-    rz0 = -.5
-    rzy = -.5
-    rzz = -.5
-    r0z = 1
-    r0y = 1
-    r0z = 1
-    exact_R2 = (w_0*(r00*exact_Phi_x_Q_0 + r0y*exact_Phi_x_Q_y + r0z*exact_Phi_x_Q_z) + w_z*(rz0*exact_Phi_z_Q_0 + rzy*exact_Phi_z_Q_y + rzz*exact_Phi_z_Q_z))/(.5)
-    avg_R2 = (w_0*(r00*avg_Phi_x_Q_0 + r0y*avg_Phi_x_Q_y + r0z*avg_Phi_x_Q_z) + w_z*(rz0*avg_Phi_z_Q_0 + rzy*avg_Phi_z_Q_y + rzz*avg_Phi_z_Q_z))/(.5)
-    ax[1].plot(t, exact_R2, color='blue', label=r'$\mathcal{R}=\langle0\vert\cdot\vert0\rangle\,\vert-\rangle\langle-\vert$', marker='s', markersize=markersize, markevery=len(t))
-    ax[1].errorbar(t, avg_R2, err_rho_S, color='blue', marker='s', markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
+# R 0 discord - p = 0.9
+p = .9
+avg_Phi_x_Q_0_0d = p*avg_Phi_0_tilde_Q_0_0d + (1-p)*avg_Phi_1_Q_0_0d
+err_Phi_x_Q_0_0d = p*err_Phi_0_tilde_Q_0_0d + (1-p)*err_Phi_1_Q_0_0d
+exact_Phi_x_Q_0_0d = p*exact_Phi_0_tilde_Q_0_0d + (1-p)*exact_Phi_1_Q_0_0d
+avg_Phi_x_Q_x_0d = p*avg_Phi_0_tilde_Q_x_0d + (1-p)*avg_Phi_1_Q_x_0d
+err_Phi_x_Q_x_0d = p*err_Phi_0_tilde_Q_x_0d + (1-p)*err_Phi_1_Q_x_0d
+exact_Phi_x_Q_x_0d = p*exact_Phi_0_tilde_Q_x_0d + (1-p)*exact_Phi_1_Q_x_0d
+avg_Phi_x_Q_y_0d = p*avg_Phi_0_tilde_Q_y_0d + (1-p)*avg_Phi_1_Q_y_0d
+err_Phi_x_Q_y_0d = p*err_Phi_0_tilde_Q_y_0d + (1-p)*err_Phi_1_Q_y_0d
+exact_Phi_x_Q_y_0d = p*exact_Phi_0_tilde_Q_y_0d + (1-p)*exact_Phi_1_Q_y_0d
+avg_Phi_z_Q_z_0d = avg_Phi_1_Q_z_0d
+err_Phi_z_Q_z_0d = err_Phi_1_Q_z_0d
+exact_Phi_z_Q_z_0d = exact_Phi_1_Q_z_0d
+exact_rho_S = w_0*exact_Phi_x_Q_0_0d + w_x*exact_Phi_x_Q_x_0d + w_y*exact_Phi_x_Q_y_0d + w_z*exact_Phi_z_Q_z_0d
+avg_rho_S = w_0*avg_Phi_x_Q_0_0d + w_x*avg_Phi_x_Q_x_0d + w_y*avg_Phi_x_Q_y_0d + w_z*avg_Phi_z_Q_z_0d
+err_rho_S = .25*np.sqrt(err_Phi_x_Q_0_0d**2 + err_Phi_x_Q_x_0d**2 + err_Phi_x_Q_y_0d**2 + err_Phi_z_Q_z_0d**2)
+ax[1].plot(t, exact_rho_S, '--', color=colors[1], label=r'$\mathcal{R}_0^{0.9}$', marker=markers[2], markersize=markersize, markevery=len(t))
+ax[1].errorbar(t, avg_rho_S, err_rho_S, color=colors[1], marker=markers[2], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
-# R[X] = <0|X|0> |0><0|
-rz0 = -.5
-rzx = -.5
-rzy = -.5
+# R factorized
 r00 = 1
 r0x = 1
 r0y = 1
-exact_R2 = (w_0*(r00*exact_Phi_x_Q_0 + r0x*exact_Phi_x_Q_x + r0y*exact_Phi_x_Q_y) + w_z*(rz0*exact_Phi_z_Q_0 + rzx*exact_Phi_z_Q_x + rzy*exact_Phi_z_Q_y))/(.5)
-avg_R2 = (w_0*(r00*avg_Phi_x_Q_0 + r0x*avg_Phi_x_Q_x + r0y*avg_Phi_x_Q_y) + w_z*(rz0*avg_Phi_z_Q_0 + rzx*avg_Phi_z_Q_x + rzy*avg_Phi_z_Q_y))/(.5)
-ax[1].plot(t, exact_R2, color=colors[6], label=r'$\mathcal{R}=\langle0\vert\cdot\vert0\rangle\,\vert0\rangle\langle0\vert$', marker=markers[2], markersize=markersize, markevery=len(t))
-ax[1].errorbar(t, avg_R2, err_rho_S, color=colors[6], marker=markers[2], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
-
-# R[X] = <1|X|1> |1><1|
-rz0 = .5
-rzx = .5
-rzy = .5
-rzz = 1
-exact_R3 = w_z*(rz0*exact_Phi_z_Q_0 + rzx*exact_Phi_z_Q_x + rzy*exact_Phi_z_Q_y + rzz*exact_Phi_z_Q_z)/(.5)
-avg_R3 = w_z*(rz0*avg_Phi_z_Q_0 + rzx*avg_Phi_z_Q_x + rzy*avg_Phi_z_Q_y + rzz*avg_Phi_z_Q_z)/(.5)
-ax[1].plot(t, exact_R3, color=colors[7], label=r'$\mathcal{R}=\langle1\vert\cdot\vert1\rangle\,\vert1\rangle\langle1\vert$', marker=markers[3], markersize=markersize, markevery=len(t))
-ax[1].errorbar(t, avg_R3, err_rho_S, color=colors[7], marker=markers[3], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
+r0z = 1
+exact_R1 = w_0*(r00*exact_Phi_x_Q_0 + r0x*exact_Phi_x_Q_y + r0y*exact_Phi_x_Q_y + r0z*exact_Phi_x_Q_z)
+avg_R1 = w_0*(r00*avg_Phi_x_Q_0 + r0x*avg_Phi_x_Q_x + r0y*avg_Phi_x_Q_y + r0z*avg_Phi_x_Q_z)
+ax[1].plot(t, exact_R1, color=colors[2], label=r'$\mathcal{R}_{\text{f}}$', marker=markers[3], markersize=markersize, markevery=len(t))
+ax[1].errorbar(t, avg_R1, err_rho_S, color=colors[2], marker=markers[3], markevery=markevery, errorevery=markevery, markersize=markersize, linewidth=0, elinewidth=1)
 
 
 
